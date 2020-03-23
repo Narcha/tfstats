1c3651a7674788a12616ded4d251793e1f8ac149
damightyzombie <42248344+DaMightyZombie@users.noreply.github.com>
2020-03-23 21:56:21 +0100

    various setup

---
+++ profiles/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the stats index.")
+
+def profile(request, steamid):
+    return HttpResponse(str(steamid))
