2cac41c00333c9ea23940cc25598d3255735b30f
damightyzombie <42248344+DaMightyZombie@users.noreply.github.com>
2020-04-11 23:25:04 +0200

    improved steam API integration, misc improvements

--- profiles/views.py
+++ profiles/views.py
@@ -1,34 +1,39 @@
 from django.utils import timezone
 from django.shortcuts import render, redirect
 from django.http import HttpResponseNotFound
-from steam_api.models import PlayerStat, PlayerProfile, validate_steamid
-import tfstats.errors
+from steam_api.models import PlayerStat, PlayerProfile
+from tfstats.errors import InvalidSteamIDError, SteamAPIError
+from steam_api.steamid import resolve_steamid_or_profile_link
 
 def index(request):
     return render(request, 'profiles_index.html', {"page_name": "Profiles"})
 
 def profile(request, steamid):
-    if not validate_steamid(steamid):
+    try:
+        resolved_id = resolve_steamid_or_profile_link(steamid)
+    except InvalidSteamIDError:
         return invalid_id(request, steamid)
+    except SteamAPIError:
+        return index(request)
 
     # Check if we have records for the given steamID already
     try:
-        db_record = PlayerStat.objects.get(steamid=steamid)
+        db_record = PlayerStat.objects.get(steamid=resolved_id)
     except PlayerStat.DoesNotExist:
         db_record = None
-    print("looked up records for %d, found %s"% (steamid, db_record))
+    print("looked up records for %d, found %s"% (resolved_id, db_record))
 
     playerstats = None
 
     if db_record is not None:
         # update existing record if that entry is older than five minutes
         if timezone.now() - db_record.timestamp > timezone.timedelta(minutes=5):
-            db_record.get_by_steamid(steamid)
+            db_record.get_by_steamid(resolved_id)
         playerstats = db_record
     else:
         # create new record
         playerstats = PlayerStat()
-        playerstats.get_by_steamid(steamid)
+        playerstats.get_by_steamid(resolved_id)
 
 
     # Serve either the new record or an old one, given that it's not older than five minutes
