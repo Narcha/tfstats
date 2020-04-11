f7194b52648cb16995438ab34ad5eefcad890d8f
damightyzombie <42248344+DaMightyZombie@users.noreply.github.com>
2020-04-11 15:50:38 +0200

    added steam API query, the profile page now displays real stats instead of dummies

--- tfstats/errors.py
+++ tfstats/errors.py
@@ -1,2 +1,8 @@
 class InvalidSteamIDError(Exception):
-    pass
+    """The given SteamID is not valid."""
+
+class SteamAPIError(Exception):
+    """The Steam API returned a response code other than 200"""
+    def __init__(self, code):
+        self.code = code
+        super().__init__()
