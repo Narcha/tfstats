31be58e79a65a3335b5d6e26b6d4503acacaceed
damightyzombie <42248344+DaMightyZombie@users.noreply.github.com>
2020-04-15 22:23:09 +0200

    added allowe hosts

--- tfstats/settings.py
+++ tfstats/settings.py
@@ -21,7 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "pc-rasmus",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
