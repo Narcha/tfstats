61730f98a3e0dd51d5035c45077a15bf48042547
damightyzombie <42248344+DaMightyZombie@users.noreply.github.com>
2020-07-29 20:40:07 +0200

    Revert "added heroku stuff"
    This reverts commit a6118336743e58bd366a743a613f5a9065d20376.

--- tfstats/settings.py
+++ tfstats/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -149,6 +148,3 @@ with open(os.path.join(BASE_DIR, "secret/SECRET_KEY"), "r") as fp:
 # https://docs.djangoproject.com/en/3.0/topics/http/sessions/
 
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
